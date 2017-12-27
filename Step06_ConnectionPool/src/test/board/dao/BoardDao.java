12a56636428687a901e76196410169a96d7f4ee2
jiehee <daily92@hanmail.net>
2017-12-27 13:19:43 +0900

    수정

--- Step06_ConnectionPool/src/test/board/dao/BoardDao.java
+++ Step06_ConnectionPool/src/test/board/dao/BoardDao.java
@@ -21,24 +21,24 @@ public class BoardDao {
 		return dao;
 	}
 	
-	//글정보를 수정하는 메소드 
+	// 글 정보를 수정하는 메소드 
 	public boolean update(BoardDto dto) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		boolean isSuccess = false;
+		Connection conn= null;
+		PreparedStatement pstmt= null;
+		boolean isSuccess= false;
 		try {
-			//Connection 객체의 참조값 얻어오기
-			conn = new DbcpBean().getConn();
-			String sql = "UPDATE board_guest "
+			// Connection 객체의 참조값 얻어오기
+			conn= new DbcpBean().getConn();
+			String sql= "UPDATE board_guest "
 					+ " SET writer=?,title=?,content=? "
 					+ " WHERE num=?";
-			pstmt = conn.prepareStatement(sql);
+			pstmt= conn.prepareStatement(sql);
 			pstmt.setString(1, dto.getWriter());
 			pstmt.setString(2, dto.getTitle());
 			pstmt.setString(3, dto.getContent());
 			pstmt.setInt(4, dto.getNum());
-			//sql 문 수행하기
-			int flag = pstmt.executeUpdate();
+			// sql 문 수행하기
+			int flag= pstmt.executeUpdate();
 			if (flag > 0) {
 				isSuccess = true;
 			}
@@ -46,39 +46,38 @@ public class BoardDao {
 			e.printStackTrace();
 		} finally {
 			try {
-				if (pstmt != null)
+				if (pstmt!= null)
 					pstmt.close();
-				if (conn != null)
+				if (conn!= null)
 					conn.close();
-			} catch (Exception e) {
-			}
+			} catch (Exception e) {}
 		}
 		return isSuccess;
 	}
 	
 	
-	//인자로 전달되는 글번호에 해당하는 정보를 리턴해주는 메소드 
+	// 인자로 전달되는 글 번호에 해당하는 정보를 리턴해주는 메소드 
 	public BoardDto getData(int num) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		BoardDto dto=null;
+		Connection conn= null;
+		PreparedStatement pstmt= null;
+		ResultSet rs= null;
+		BoardDto dto= null;
 		try {
-			conn = new DbcpBean().getConn();
-			String sql = "SELECT writer,title,content,regdate"
+			conn= new DbcpBean().getConn();
+			String sql= "SELECT writer,title,content,regdate"
 					+ " FROM board_guest "
 					+ " WHERE num=?";
-			pstmt = conn.prepareStatement(sql);
+			pstmt= conn.prepareStatement(sql);
 			pstmt.setInt(1, num);
-			//sql 문 수행하고 결과셋 받아오기 
-			rs = pstmt.executeQuery();
+			// sql 문 수행하고 결과셋 받아오기 
+			rs= pstmt.executeQuery();
 			if (rs.next()) {
-				String writer=rs.getString("writer");
-				String title=rs.getString("title");
-				String content=rs.getString("content");
-				String regdate=rs.getString("regdate");
-				//글정보를 BoardDto 에 담는다.
-				dto=new BoardDto();
+				String writer= rs.getString("writer");
+				String title= rs.getString("title");
+				String content= rs.getString("content");
+				String regdate= rs.getString("regdate");
+				// 글 정보를 BoardDto 에 담는다.
+				dto= new BoardDto();
 				dto.setNum(num);
 				dto.setWriter(writer);
 				dto.setTitle(title);
@@ -89,11 +88,11 @@ public class BoardDao {
 			e.printStackTrace();
 		} finally {
 			try {
-				if (rs != null)
+				if (rs!= null)
 					rs.close();
-				if (pstmt != null)
+				if (pstmt!= null)
 					pstmt.close();
-				if (conn != null)
+				if (conn!= null)
 					conn.close();
 			} catch (Exception e) {
 			}
@@ -101,29 +100,29 @@ public class BoardDao {
 		return dto;
 	}
 	
-	//글정보를 삭제하는 메소드
+	// 글 정보를 삭제하는 메소드
 	public boolean delete(int num) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		boolean isSuccess = false;
+		Connection conn= null;
+		PreparedStatement pstmt= null;
+		boolean isSuccess= false;
 		try {
-			//Connection 객체의 참조값 얻어오기
-			conn = new DbcpBean().getConn();
-			String sql = "DELETE FROM board_guest WHERE num=?";
-			pstmt = conn.prepareStatement(sql);
+			// Connection 객체의 참조값 얻어오기
+			conn= new DbcpBean().getConn();
+			String sql= "DELETE FROM board_guest WHERE num=?";
+			pstmt= conn.prepareStatement(sql);
 			pstmt.setInt(1, num);
-			//sql 문 수행하기
-			int flag = pstmt.executeUpdate();
+			// sql 문 수행하기
+			int flag= pstmt.executeUpdate();
 			if (flag > 0) {
-				isSuccess = true;
+				isSuccess= true;
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			try {
-				if (pstmt != null)
+				if (pstmt!= null)
 					pstmt.close();
-				if (conn != null)
+				if (conn!= null)
 					conn.close();
 			} catch (Exception e) {
 			}
@@ -132,33 +131,33 @@ public class BoardDao {
 	}
 	
 	
-	//글 목록을 리턴해주는 메소드
+	// 글 목록을 리턴해주는 메소드
 	public List<BoardDto> getList(){
-		Connection conn=null;
-		PreparedStatement pstmt=null;
-		ResultSet rs=null;
-		//글 목록을 저장할 객체 생성 
-		List<BoardDto> list=new ArrayList<>();
+		Connection conn= null;
+		PreparedStatement pstmt= null;
+		ResultSet rs= null;
+		// 글 목록을 저장할 객체 생성 
+		List<BoardDto> list= new ArrayList<>();
 		try{
-			conn=new DbcpBean().getConn();
-			String sql="SELECT num,writer,title,regdate"
+			conn= new DbcpBean().getConn();
+			String sql= "SELECT num,writer,title,regdate"
 					+ " FROM board_guest"
 					+ " ORDER BY num DESC";
-			pstmt=conn.prepareStatement(sql);
-			//sql 문 수행하고 결과셋 받아오기 
-			rs=pstmt.executeQuery();
+			pstmt= conn.prepareStatement(sql);
+			// sql 문 수행하고 결과셋 받아오기 
+			rs= pstmt.executeQuery();
 			while(rs.next()){
-				int num=rs.getInt("num");
-				String writer=rs.getString("writer");
-				String title=rs.getString("title");
-				String regdate=rs.getString("regdate");
-				//글정보를 BoardDto 에 담아서
-				BoardDto dto=new BoardDto();
+				int num= rs.getInt("num");
+				String writer= rs.getString("writer");
+				String title= rs.getString("title");
+				String regdate= rs.getString("regdate");
+				// 글 정보를 BoardDto 에 담아서
+				BoardDto dto= new BoardDto();
 				dto.setNum(num);
 				dto.setWriter(writer);
 				dto.setTitle(title);
 				dto.setRegdate(regdate);
-				//List<BoardDto> 객체에 저장한다.
+				// List<BoardDto> 객체에 저장한다.
 				list.add(dto);
 			}
 		}catch(Exception e){
@@ -171,27 +170,27 @@ public class BoardDao {
 				if(conn!=null)conn.close();
 			}catch(Exception e){}
 		}
-		//글목록을 리턴해준다. 
+		// 글 목록을 리턴해준다. 
 		return list;
 	}
-	//글 하나의 정보를 저장하는 메소드
+	// 글 하나의 정보를 저장하는 메소드
 	public boolean insert(BoardDto dto) {
-		Connection conn=null;
-		PreparedStatement pstmt=null;
-		boolean isSuccess=false;
+		Connection conn= null;
+		PreparedStatement pstmt= null;
+		boolean isSuccess= false;
 		try{
-			//Connection 객체의 참조값 얻어오기
-			conn=new DbcpBean().getConn();
-			String sql="INSERT INTO board_guest "
+			// Connection 객체의 참조값 얻어오기
+			conn= new DbcpBean().getConn();
+			String sql= "INSERT INTO board_guest "
 					+ "(num,writer,title,content,regdate) "
 				+ "VALUES(board_guest_seq.NEXTVAL,?,?,?,SYSDATE)";
-			pstmt=conn.prepareStatement(sql);
-			//? 에 값 바인딩하기
+			pstmt= conn.prepareStatement(sql);
+			// ? 에 값 바인딩하기
 			pstmt.setString(1, dto.getWriter());
 			pstmt.setString(2, dto.getTitle());
 			pstmt.setString(3, dto.getContent());
-			//sql 문 수행하기
-			int flag=pstmt.executeUpdate();
+			// sql 문 수행하기
+			int flag= pstmt.executeUpdate();
 			if(flag>0){
 				isSuccess=true;
 			}
@@ -200,8 +199,8 @@ public class BoardDao {
 		}finally{
 			try{
 				if(pstmt!=null)pstmt.close();
-				//Connection 객체의 .close() 메소드 호출하면 
-				//Connection 객체가 알아서 Pool 에 반납된다. 
+				// Connection 객체의 .close() 메소드 호출하면 
+				// Connection 객체가 알아서 Pool 에 반납된다. 
 				if(conn!=null)conn.close();
 			}catch(Exception e){}
 		}
